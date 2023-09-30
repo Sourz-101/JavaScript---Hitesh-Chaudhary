6457ff463cf10c591e4b82f1acf7fd740a64dcaf
Sourabh <sourabh4pvt@gmail.com>
2023-10-01 00:01:43 +0530

    Day 1: video (1 - 8) Time: 1hr 40min approx

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const fName = 'Sourabh';
+let accNo = '184315984';
+var phNo = '9003749275';
+
+let DOB; // if we do not define a variale then the value of the variable is undefined
+
+
+city = 'Delhi'; // we can also declair variable  like this but we should avoid this
+
+console.log(typeof(city));
+console.table([fName, accNo, phNo, city, DOB]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //this is used to treat all JS code as a newer version || Not required to write this as it is already in the V8 engine by degault.    
+
+
+/*
+DATA TYPES IN JAVASCRIT
+
+    
+    // PRIMITIYE DATA TYPES
+    
+    Number => -(2⁵³ - 1) < number < (2⁵³ - 1)
+    bigint 
+    string => ""
+    boolean => true / false
+    null => standalone value
+    undefined => 
+    symbol => unique
+
+*/
+
+console.log(typeof(null));      //Object
+console.log(typeof(undefined)); //undefined
+
+// OBJECTS 
+
+    
